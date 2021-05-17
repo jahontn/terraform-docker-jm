48c4718530852301fd8e0bd0ae75405dfc356ba3
Ubuntu <ubuntu@ip-172-31-2-201.ec2.internal>
2021-05-17 04:29:06 +0000

    created image nodered

---
+++ main.tf
@@ -0,0 +1,13 @@
+terraform {
+  required_providers {
+    docker = {
+      source = "kreuzwerker/docker"
+    }
+  }
+}
+
+provider "docker" {}
+
+resource "docker_image" "nodered_image" {
+  name = "nodered/node-red:latest"
+}

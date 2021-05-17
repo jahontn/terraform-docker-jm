77b83f2a77a557b4548ad53612b87e78ad418e84
root <root@ip-172-31-2-201.ec2.internal>
2021-05-17 05:44:52 +0000

    random string resource added

--- main.tf
+++ main.tf
@@ -12,12 +12,33 @@ resource "docker_image" "nodered_image" {
   name = "nodered/node-red:latest"
 }
 
+resource "random_string" "random" {
+    length = 4
+    special = false
+    upper = false
+}
+
+resource "random_string" "random2" {
+    length = 4
+    special = false
+    upper = false
+}
+
 resource "docker_container" "nodered_container" {
-  name  = "nodered"
+  name  = join("-",["nodered", random_string.random.result])
   image = docker_image.nodered_image.latest
   ports {
     internal = 1880
-    external = 1880
+    #external = 1880
+  }
+}
+
+resource "docker_container" "nodered_container2" {
+  name  = join("-", ["nodered", random_string.random2.result])
+  image = docker_image.nodered_image.latest
+  ports {
+    internal = 1880
+    #external = 1880
   }
 }
 
@@ -30,4 +51,14 @@ output "container-name" {
     value = docker_container.nodered_container.name
     description = "The name of the container"
     
+}
+
+output "IP-Address2" {
+    value = join(":", [docker_container.nodered_container.ip_address, docker_container.nodered_container.ports[0].external])
+    description = "The Ip address and external port of the container"
+}
+
+output "container-name2" {
+    value = docker_container.nodered_container2.name
+    description = "The name of the second container"
 }
